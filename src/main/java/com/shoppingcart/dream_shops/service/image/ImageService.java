718b63c37dd14cbfde6b1305617c973578c248b6
ghislaingenay <ghislain.gny@gmail.com>
2025-06-10 16:55:19 +0700

    Add ImageRepository interface and update ImageService with dependencies

--- src/main/java/com/shoppingcart/dream_shops/service/image/ImageService.java
+++ src/main/java/com/shoppingcart/dream_shops/service/image/ImageService.java
@@ -1,10 +1,20 @@
 package com.shoppingcart.dream_shops.service.image;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.shoppingcart.dream_shops.model.Image;
+import com.shoppingcart.dream_shops.repository.ImageRepository;
+import com.shoppingcart.dream_shops.service.product.IProductService;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ImageService implements IImageService {
+
+  private final ImageRepository imageRepository;
+  private final IProductService productService;
 
   @Override
   public Image getImageById(Long id) {
