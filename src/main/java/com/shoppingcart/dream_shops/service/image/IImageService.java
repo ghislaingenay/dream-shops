b45020816a1af89824fab03b8a64089bcbbb7052
ghislaingenay <ghislain.gny@gmail.com>
2025-06-10 16:51:30 +0700

    Implement ImageService class and IImageService interface for image management

--- src/main/java/com/shoppingcart/dream_shops/service/image/IImageService.java
+++ src/main/java/com/shoppingcart/dream_shops/service/image/IImageService.java
@@ -1,5 +1,16 @@
 package com.shoppingcart.dream_shops.service.image;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.shoppingcart.dream_shops.model.Image;
+
 public interface IImageService {
+  Image getImageById(Long id);
+
+  void deleteImageById(Long id);
+
+  Image saveImage(Long productId, MultipartFile file);
+
+  void updateImage(Long imageId, MultipartFile file);
 
 }
--- src/main/java/com/shoppingcart/dream_shops/service/image/ImageService.java
+++ src/main/java/com/shoppingcart/dream_shops/service/image/ImageService.java
@@ -1,5 +1,28 @@
 package com.shoppingcart.dream_shops.service.image;
 
-public class ImageService {
+import org.springframework.web.multipart.MultipartFile;
+
+import com.shoppingcart.dream_shops.model.Image;
+
+public class ImageService implements IImageService {
+
+  @Override
+  public Image getImageById(Long id) {
+    return null;
+  }
+
+  @Override
+  public Image saveImage(Long productId, MultipartFile image) {
+    return null;
+  }
+
+  @Override
+  public void updateImage(Long imageId, MultipartFile file) {
+
+  }
+
+  @Override
+  public void deleteImageById(Long id) {
+  }
 
 }
