b87f4530a8da45f77cf78d489f36ba44c5a3341b
ghislaingenay <ghislain.gny@gmail.com>
2025-06-12 13:08:47 +0700

    Add Order and OrderItem classes with relationships and additional fields

--- src/main/java/com/shoppingcart/dream_shops/model/Order.java
+++ src/main/java/com/shoppingcart/dream_shops/model/Order.java
@@ -1,9 +1,19 @@
 package com.shoppingcart.dream_shops.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.shoppingcart.dream_shops.enums.OrderStatus;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,5 +28,14 @@ public class Order {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generates a unique identifier for each category
   private long id;
+
+  private LocalDate orderDate; // Date when the order was placed
+  private BigDecimal totalAmount; // Total amount for the order
+  @Enumerated(value = EnumType.STRING) // Store enum as a string in the database
+  private OrderStatus orderStatus; // Status of the order (e.g., "Pending", "Shipped", "Delivered")
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true) // Maps the relationship to the
+                                                                                  // OrderItem entity
+  private Set<OrderItem> orderItems; // Set of items in the order
 
 }
--- src/main/java/com/shoppingcart/dream_shops/model/OrderItem.java
+++ src/main/java/com/shoppingcart/dream_shops/model/OrderItem.java
@@ -1,10 +1,13 @@
 package com.shoppingcart.dream_shops.model;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,11 +15,29 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+// @AllArgsConstructor
 @Entity
 public class OrderItem {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generates a unique identifier for each category
   private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id", nullable = false)
+  private Order order; // Reference to the order this item belongs to
+
+  @ManyToOne
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product; // Reference to the order this item belongs to
+
+  private int quantity;
+  private BigDecimal price;
+
+  public OrderItem(Order order, Product product, BigDecimal price, int quantity) {
+    this.price = price;
+    this.quantity = quantity;
+    this.order = order;
+    this.product = product;
+  }
 
 }
