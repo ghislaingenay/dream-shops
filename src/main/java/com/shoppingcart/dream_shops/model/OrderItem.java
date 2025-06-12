aef06b4322b6b5f475f129ab9431ec54b0418697
ghislaingenay <ghislain.gny@gmail.com>
2025-06-12 12:31:37 +0700

    Add Order and OrderItem entity classes with autogenerated IDs

---
+++ src/main/java/com/shoppingcart/dream_shops/model/OrderItem.java
@@ -0,0 +1,22 @@
+package com.shoppingcart.dream_shops.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class OrderItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generates a unique identifier for each category
+  private long id;
+
+}
