904a6e160cac82cc830b724b9327b0be1e18b95b
ghislaingenay <ghislain.gny@gmail.com>
2025-06-12 10:51:03 +0700

    Implement methods in CartController for adding, removing, and updating item quantities in the cart; integrate with ICarItemService

--- src/main/java/com/shoppingcart/dream_shops/controller/CartController.java
+++ src/main/java/com/shoppingcart/dream_shops/controller/CartController.java
@@ -6,11 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shoppingcart.dream_shops.model.Cart;
 import com.shoppingcart.dream_shops.response.ApiResponse;
+import com.shoppingcart.dream_shops.service.car_item.ICarItemService;
 import com.shoppingcart.dream_shops.service.cart.ICartService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("${api.prefix}/carts")
 public class CartController {
   private final ICartService cartService;
+  private final ICarItemService cartItemService;
 
   @GetMapping("/{cartId}")
   public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
@@ -40,6 +45,26 @@ public class CartController {
     // Implementation to retrieve cart by user ID
     BigDecimal totalPrice = cartService.getTotalPrice(cartId);
     return ResponseEntity.ok(new ApiResponse("Total Price", totalPrice));
+  }
+
+  @PostMapping("/{cartId}/cart/items/{productId}/add")
+  public ResponseEntity<ApiResponse> addItemToCart(@PathVariable Long cartId, @PathVariable Long productId,
+      @RequestParam int quantity) {
+    cartItemService.addItemToCart(cartId, productId, quantity);
+    return ResponseEntity.ok(new ApiResponse("Product added to cart", true));
+  }
+
+  @DeleteMapping("/{cartId}/cart/items/{productId}/remove")
+  public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long productId) {
+    cartItemService.removeItemFromCart(cartId, productId);
+    return ResponseEntity.ok(new ApiResponse("Product removed from cart", true));
+  }
+
+  @PutMapping("/{cartId}/cart/items/${itemId}/update")
+  public ResponseEntity<ApiResponse> updateItemQuantity(@PathVariable Long cartId, @PathVariable Long itemId,
+      @RequestParam int quantity) {
+    cartItemService.updateItemQuantity(cartId, itemId, quantity);
+    return ResponseEntity.ok(new ApiResponse("Product quantity updated", null));
   }
 
 }
