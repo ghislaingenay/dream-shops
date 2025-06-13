e4b78d6fe36498a92197fe54309d3415c20db8a6
ghislaingenay <ghislain.gny@gmail.com>
2025-06-13 12:46:39 +0700

    Add UnAuthorizedHttpException and UserService implementation

---
+++ src/main/java/com/shoppingcart/dream_shops/service/user/IUserService.java
@@ -0,0 +1,17 @@
+package com.shoppingcart.dream_shops.service.user;
+
+import com.shoppingcart.dream_shops.model.User;
+import com.shoppingcart.dream_shops.request.CreateUserRequest;
+import com.shoppingcart.dream_shops.request.UpdateUserRequest;
+
+public interface IUserService {
+  User getUserById(Long userId);
+
+  User getUserByEmail(String email);
+
+  User createUser(CreateUserRequest request);
+
+  User updateUser(Long userId, UpdateUserRequest user);
+
+  void deleteUser(Long userId);
+}
