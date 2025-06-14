36ebd185e5ef0a964ea240572a8027445f9bb1fe
ghislaingenay <ghislain.gny@gmail.com>
2025-06-14 09:37:03 +0700

    feat: implement UserDetails interface in ShopUserDetails class with proper authority handling

--- src/main/java/com/shoppingcart/dream_shops/security/user/ShopUserDetails.java
+++ src/main/java/com/shoppingcart/dream_shops/security/user/ShopUserDetails.java
@@ -1,11 +1,39 @@
 package com.shoppingcart.dream_shops.security.user;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.shoppingcart.dream_shops.model.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShopUserDetails implements UserDetails {
+  private Long id;
+  private String email;
+  private String password;
+
+  private Collection<GrantedAuthority> authorities;
+
+  public static ShopUserDetails buildUserDetails(User user) {
+    List<GrantedAuthority> authorities = user.getRoles().stream()
+        .map(role -> new SimpleGrantedAuthority(role.getName()))
+        .collect(Collectors.toList());
+
+    return new ShopUserDetails(user.getId(), user.getEmail(), user.getPassword(), authorities);
+
+  }
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -15,14 +43,39 @@ public class ShopUserDetails implements UserDetails {
 
   @Override
   public String getPassword() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    return password;
   }
 
   @Override
   public String getUsername() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    return email;
   }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
+
+  // @Override
+  // public boolean equals(Object o) {
+  // if (this == o) return true;
+  // if (!(o instanceof ShopUserDetails)) return false;
+  // ShopUserDetails that = (ShopUserDetails) o;
+  // return id != null && id.equals(that.id);
 
 }
