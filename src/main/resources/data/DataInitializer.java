7df7c40bad2140c59c272397aef00db7d1464759
ghislaingenay <ghislain.gny@gmail.com>
2025-06-14 13:39:04 +0700

    feat: add DataInitializer to initialize categories and products on application startup

---
+++ src/main/resources/data/DataInitializer.java
@@ -0,0 +1,61 @@
+package data;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import com.shoppingcart.dream_shops.model.Category;
+import com.shoppingcart.dream_shops.model.Product;
+import com.shoppingcart.dream_shops.repository.CategoryRepository;
+import com.shoppingcart.dream_shops.repository.ProductRepository;
+import com.shoppingcart.dream_shops.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+
+  private final CategoryRepository categoryRepository;
+  private final ProductRepository productRepository;
+  private final UserRepository userRepository;
+
+  public DataInitializer(CategoryRepository categoryRepository, ProductRepository productRepository,
+      UserRepository userRepository) {
+    this.categoryRepository = categoryRepository;
+    this.productRepository = productRepository;
+    this.userRepository = userRepository;
+  }
+
+  // private void createUserIfNotExists(String username, String password) {
+  // if (!userRepository.findBy('email', username)) {
+  // User user = new User();
+  // user.setUsername(username);
+  // user.setPassword(passwordEncoder.encode(&é"")); // In a real application,
+  // ensure to hash the
+  // password
+  // userRepository.save(user);
+  // }
+  // }
+
+  @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        // Initialize categories
+        if (!categoryRepository.existsByName("Electronics")) {
+            Category electronics = new Category();
+            electronics.setName("Electronics");
+            categoryRepository.save(electronics);
+        }
+
+        // Initialize products
+        if (!productRepository.existsByNameAndBrand("Electronics", "Apple")) {
+            Product smartphone = new Product();
+            smartphone.setName("Smartphone");
+            smartphone.setCategory(categoryRepository.findByName("Electronics"));
+            productRepository.save(smartphone);
+        }
+
+        // Initialize users
+        
+
+}
