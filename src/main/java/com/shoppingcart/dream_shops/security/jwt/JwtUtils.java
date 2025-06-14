e3bd59ee8c6a1c4671c3384be77a6efbc960761c
ghislaingenay <ghislain.gny@gmail.com>
2025-06-14 11:31:27 +0700

    feat: implement JwtUtils class for JWT token generation and validation

---
+++ src/main/java/com/shoppingcart/dream_shops/security/jwt/JwtUtils.java
@@ -0,0 +1,62 @@
+package com.shoppingcart.dream_shops.security.jwt;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import com.shoppingcart.dream_shops.security.user.ShopUserDetails;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+
+public class JwtUtils {
+  private String jwtSecret;
+  private int expirationTime;
+
+  private Key key() {
+    return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+  }
+
+  @SuppressWarnings("deprecation")
+  public String generateTokenForUser(Authentication authentication) {
+    ShopUserDetails userPrincipal = (ShopUserDetails) authentication.getPrincipal();
+    List<String> roles = userPrincipal.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.toList());
+    return Jwts.builder()
+        .subject(userPrincipal.getUsername())
+        .claim("id", userPrincipal.getId())
+        .claim("roles", roles)
+        .issuedAt(new Date())
+        .expiration(new Date((new Date().getTime()) + expirationTime))
+        .signWith(key(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  public String getUsernameFromToken(String token) {
+    return Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(token)
+        .getPayload().getSubject();
+  }
+
+  public boolean validateToken(String token) {
+    try {
+      Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(token);
+      return true;
+    } catch (ExpiredJwtException | UnsupportedJwtException | SignatureException | MalformedJwtException
+        | IllegalArgumentException e) {
+      return false;
+    }
+  }
+}
